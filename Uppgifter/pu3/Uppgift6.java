b3defb318b3238308aaec7c51d8f5a5ad1d86b85
VadymNovoselskyi <novoselskyi.vadim@gmail.com>
2023-09-13 11:41:07 +0200

    add hw from 12/09

--- Uppgifter/pu3/Uppgift6.java
+++ Uppgifter/pu3/Uppgift6.java
@@ -10,7 +10,10 @@ public class Uppgift6 {
 		double[] number = new double[100];
 		
 		int choise;
-		int i = 0;
+		int count = 0;
+		double sum = 0;
+		double min = 0;
+		double max = 0;
 		boolean run = true;
 		
 		do {
@@ -22,22 +25,32 @@ public class Uppgift6 {
 			choise = input.nextInt();
 
 			if(choise == 1) {
-				number[i] = input.nextDouble();
-				i++;
+				System.out.println("Enter new number");
+				number[count] = input.nextDouble();
+				count++;
 			}
 			else if(choise == 2) {
-				
+				for(int i = 0; i <= count; i++) {
+					sum += number[i];
+				}
+				System.out.println("Averages of the numbers is " +sum/count);
 			}
 			else if(choise == 3) {
-				
+				for(int i = 0; i <= count; i++) {
+					if(number[i] < min) {
+						min = number[i];
+					}
+					else if(number[i] > max) {
+						max = number[i];
+					}
+				}
+				System.out.println("Variation width is " +(max-min));
+			}
+			else if(choise == 4) {
+				run = false;
 			}
 			else {
-				
-			}
-			
-			
-			if (choise.equalsIgnoreCase("no")) {
-				run = false;
+				System.out.println("Wrong value");
 			}
 			
 		} while(run);
