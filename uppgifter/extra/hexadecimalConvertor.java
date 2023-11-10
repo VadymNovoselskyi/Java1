2d70f59bc066a87afac75c2d59d8e509ae5c0a1f
VadymNovoselskyi <novoselskyi.vadim@gmail.com>
2023-11-10 14:54:03 +0100

    add 10/11 lesson

--- uppgifter/extra/hexadecimalConvertor.java
+++ uppgifter/extra/hexadecimalConvertor.java
@@ -7,10 +7,11 @@ public class hexadecimalConvertor {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		System.out.println("Enter an integer to convert it to binary code");
+		System.out.println("Enter an integer to convert it to hexadecimal");
 		
 		int integer = input.nextInt(); //heltal
-		char[] hexadecimal = new char[50]; //array för hexadecimal form av 'integer'
+		int lengthOfNumber = (int)(Math.log10(integer) / Math.log10(16)) + 1; //antal siffror som behövs för att skriva 'integer' i bas 16
+		char[] hexadecimal = new char[lengthOfNumber]; //array för hexadecimal form av 'integer'
 
 		int exponent; //lagrar störst exponent till potens med bas 16 som är lika med eller midre än 'integer'
 		int multiple; //lagrar störst multipel till potens med bas 16 och exponent = 'exponent' så att produkten blir mindre eller lika med 'integer'
@@ -24,17 +25,18 @@ public class hexadecimalConvertor {
 			exponent = (int)(Math.log10(integer) / Math.log10(16)); //beräkna störts exponent till potens som är mindre eller lika med 'integer'
 			multiple = (int)(integer / Math.pow(16, exponent)); //beräkna störst multipel till potens som är mindre eller lika med 'integer'
 			
-			if(multiple < 10) {
-				hexadecimal[placeInHexadecimal] = String.format("%d", multiple).charAt(0);
+			if(multiple < 10) { //om multipel är en sifra (0 - 9)
+				hexadecimal[placeInHexadecimal] = String.format("%d", multiple).charAt(0); //sätta in 'multiple' i en char arrray
 			}
-			else {
-				hexadecimal[placeInHexadecimal] = (char)(multiple + 55);
+			else { //om multipel är en bokstav (A - G)
+				hexadecimal[placeInHexadecimal] = (char)(multiple + 55); //omvnandla multipel till en stor bokstav
 			}
 			
 			integer -= multiple * Math.pow(16, exponent);
 			
-			for(int i = 1; (int)Math.pow(16, exponent - i) > integer; i++) { //om ny 'integer' är mindre än 16^exponent så måste ställas noll på plats av det exponent(3)
-				placeInHexadecimal++; //sätta 0 på plats 
+			for(int i = 1; (int)Math.pow(16, exponent - i) > integer; i++) { //om ny 'integer' är mindre än 16^exponent så måste ställas noll på plats av det exponent
+				placeInHexadecimal++;
+				hexadecimal[placeInHexadecimal] = String.format("%d", 0).charAt(0); //sätta in 0 på plats
 			}
 
 			placeInHexadecimal++;
@@ -44,8 +46,8 @@ public class hexadecimalConvertor {
 			}
 		} while(run);
 
-		for(int i = 0; i < placeInHexadecimal; i++) { //utmatning av binar form
-			System.out.print(hexadecimal[i]);
+		for(char a :hexadecimal) { //utmatning av 'integer' i hexadecimal form
+			System.out.print(a);
 		}
 		
 	}
--- uppgifter/puMetoder/Uppgift11.java
+++ uppgifter/puMetoder/Uppgift11.java
@@ -59,10 +59,11 @@ public class Uppgift11 {
 			System.out.print(a);
 		}
 	}
+
 	public static char[] baseConvertor(int number, int base) {
 
-		int lengthOfNumber = (int)(Math.log10(number) / Math.log10(base));
-		char[] newBase = new char[lengthOfNumber + 1]; //array för hexadecimal form av 'integer'
+		int lengthOfNumber = (int)(Math.log10(number) / Math.log10(base)) + 1;
+		char[] newBase = new char[lengthOfNumber]; //array för hexadecimal form av 'integer'
 
 		int exponent; //lagrar störst exponent till potens med bas 16 som är lika med eller midre än 'integer'
 		int multiple; //lagrar störst multipel till potens med bas 16 och exponent = 'exponent' så att produkten blir mindre eller lika med 'integer'
@@ -76,17 +77,18 @@ public class Uppgift11 {
 			exponent = (int)(Math.log10(number) / Math.log10(base)); //beräkna störts exponent till potens som är mindre eller lika med 'integer'
 			multiple = (int)(number / Math.pow(base, exponent)); //beräkna störst multipel till potens som är mindre eller lika med 'integer'
 
-			if(multiple < 10) {
-				newBase[placeInNewBase] = String.format("%d", multiple).charAt(0);
+			if(multiple < 10) { //om multipel är en sifra (0 - 9)
+				newBase[placeInNewBase] = String.format("%d", multiple).charAt(0); //sätta in 'multiple' i en char arrray
 			}
-			else {
-				newBase[placeInNewBase] = (char)(multiple + 55);
+			else { //om multipel är en bokstav (A - G)
+				newBase[placeInNewBase] = (char)(multiple + 55); //omvnandla multipel till en stor bokstav
 			}
 
 			number -= multiple * Math.pow(base, exponent);
 
-			for(int i = 1; (int)Math.pow(base, exponent - i) > number; i++) { //om ny 'integer' är mindre än 16^exponent så måste ställas noll på plats av det exponent(3)
-				placeInNewBase++; //sätta 0 på plats 
+			for(int i = 1; (int)Math.pow(base, exponent - i) > number; i++) { //om ny 'integer' är mindre än 16^exponent så måste ställas noll på plats av det exponent
+				placeInNewBase++;
+				newBase[placeInNewBase] = String.format("%d", 0).charAt(0); //sätta in 0 på plats
 			}
 
 			placeInNewBase++;
